91e4a7e95d9e3b37064df201f0f76ca1c4d80733
Koray Kurtoglu <coreykurtoglu@gmail.com>
2023-07-25 22:02:03 -0500

    Routers

--- 3.2 HTTP Request/index.js
+++ 3.2 HTTP Request/index.js
@@ -3,7 +3,15 @@ const app = express();
 const port = 8000;
 
 app.get("/", (req, res) => {
-  res.send("Server loading...");
+  res.send("<h1>Hello Austin, Tx.</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>Koray Adams</h1><h4>Jr. Software Developer</h4>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>2405 Montopolis Dr, Austin, Tx.</h1>");
 });
 
 app.listen(port, () => {
