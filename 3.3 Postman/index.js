7c82df04da260a45eef076996b0366e44fcee9be
Koray Kurtoglu <coreykurtoglu@gmail.com>
2023-07-27 16:39:27 -0500

    Get, Post, Put, Patch, Delete

--- 3.3 Postman/index.js
+++ 3.3 Postman/index.js
@@ -18,15 +18,15 @@ app.post("/register", (req, res) => {
   res.sendStatus(201);
 });
 
-app.put("/user/angela", (req, res) => {
+app.put("/user/koray", (req, res) => {
   res.sendStatus(200);
 });
 
-app.patch("/user/angela", (req, res) => {
+app.patch("/user/koray", (req, res) => {
   res.sendStatus(200);
 });
 
-app.delete("/user/angela", (req, res) => {
+app.delete("/user/koray", (req, res) => {
   //Deleting
   res.sendStatus(200);
 });
